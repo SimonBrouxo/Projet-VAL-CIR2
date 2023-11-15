3c98647760da6872690c77fc6f841b2c385f8ded
SimonBrouxo <112818567+SimonBrouxo@users.noreply.github.com>
2023-11-15 21:11:28 +0100

    Ajoutez des fichiers projet.

---
+++ Projet-VAL-CIR2/Projet-VAL-CIR2.cpp
@@ -0,0 +1,12 @@
+﻿// Projet-VAL-CIR2.cpp : définit le point d'entrée de l'application.
+//
+
+#include "Projet-VAL-CIR2.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
